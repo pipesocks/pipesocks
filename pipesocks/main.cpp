defbdedb6b9342152f6c9cbc794c72ee8e7d4e38
yvbbrjdr <xyyqzy@hotmail.com>
2017-01-18 22:23:31 +0800

    Change 2016 to 2017

--- pipesocks/main.cpp
+++ pipesocks/main.cpp
@@ -1,6 +1,6 @@
 /*
 This file is part of pipesocks. Pipesocks is a pipe-like SOCKS5 tunnel system.
-Copyright (C) 2016  yvbbrjdr
+Copyright (C) 2017  yvbbrjdr
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
