9a6494b54710f438785693f6d056b70c2bd71834
yvbbrjdr <xyyqzy@hotmail.com>
2017-02-15 15:05:15 +0800

    The transparency under linux and Windows sucks. close #12

--- pipesocks/mainform.cpp
+++ pipesocks/mainform.cpp
@@ -51,7 +51,6 @@ MainForm::MainForm(QObject *rootObject,QObject *parent):QObject(parent) {
     connect(trayicon,SIGNAL(activated(QSystemTrayIcon::ActivationReason)),window,SLOT(requestActivate()));
     if (QSysInfo::macVersion()==QSysInfo::MV_None) {
         window->setProperty("color",QColor(48,48,48));
-        window->setProperty("opacity",0.8);
     }
     settings=new QSettings("yvbbrjdr","pipesocks",this);
     if (settings->contains("pipesocks/version")&&Version::CheckVersion(settings->value("pipesocks/version").toString())) {
